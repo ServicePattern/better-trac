984cd9831f94ea755b35a0647664cf7c44a4f789
Nikolai Maslak <nikolai.maslak@brightpattern.com>
2026-07-08 15:51:31 -0700

    Dont paster previews for images already used in ticket description or comments

--- src/attachments/imagePreview.ts
+++ src/attachments/imagePreview.ts
@@ -1,6 +1,10 @@
 import { createLayoutFromString } from "../utils/domUtils";
 
 export function pasteImagePreview(attachmentLinkEl: HTMLAnchorElement, attachmentUrl: string) {
+    if (isInDescriptionOrComments(attachmentUrl)) {
+        return
+    }
+
     const imageEl = createLayoutFromString(
         `<img
             style="
@@ -14,4 +18,27 @@ export function pasteImagePreview(attachmentLinkEl: HTMLAnchorElement, attachmen
 
     attachmentLinkEl.parentElement?.insertBefore(imageEl, attachmentLinkEl)
     console.log('Better trac: image added', imageEl);
+}
+
+
+/**
+ * Dont paste preview if the image is already used in ticket description or in a comment
+ * Both comment and description example https://trac.brightpattern.com/ticket/47053
+*/
+function isInDescriptionOrComments(attachmentUrl: string) {
+    const ticketbox = document.getElementById('ticketbox')
+
+    const isInDescription = Array.from(ticketbox?.querySelectorAll('img') || [])
+        .find(img => img.src === attachmentUrl)
+
+    if (isInDescription) {
+        return false
+    }
+
+    const changelog = document.getElementById('changelog')
+
+    const isInComments = Array.from(changelog?.querySelectorAll('img') || [])
+        .find(img => img.src === attachmentUrl)
+
+    return isInComments
 }
